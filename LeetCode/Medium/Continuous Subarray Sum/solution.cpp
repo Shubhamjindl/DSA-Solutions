2c5e5bb092237ae836c8c1f7e83474fed6e4ed01
Shubhamjindl <shubhamjindl@gmail.com>
2026-07-26 16:47:37 +0530

    [Syncode] LeetCode/Medium/Continuous Subarray Sum

---
+++ LeetCode/Medium/Continuous Subarray Sum/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int>mp;
+        int sum = 0;
+        mp[0] = -1;
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            int currRem = sum % k;
+            if(mp.find(currRem) != mp.end()){
+                if(i - mp[currRem] >= 2){
+                    return true;
+                }
+            }
+            if(mp.find(currRem) == mp.end()){
+                mp[currRem] = i;
+            }
+        }
+        return false;
+    }
+};
